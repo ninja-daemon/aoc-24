673c3c164f337b0ccacdc2291036637bb2431a9b
Jonathan Blanco Rodriguez <jblarod@mercadona.es>
2024-12-03 09:30:13 +0100

    Day 3 part 2 solution

--- day-03/main.go
+++ day-03/main.go
@@ -22,6 +22,10 @@ func main() {
 
 	fmt.Println("Part 1 solution:", solution1)
 
+	solution2 := part2Solution(memory)
+
+	fmt.Println("Part 2 solution:", solution2)
+
 }
 
 func part1Solution(memory string) int {
@@ -43,6 +47,48 @@ func part1Solution(memory string) int {
 			result, _ := evaluateMulExpression(matches[i])
 			resultsCh <- result
 		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultsCh)
+	}()
+
+	for result := range resultsCh {
+		count += result
+	}
+	return count
+}
+
+func part2Solution(memory string) int {
+	re := regexp.MustCompile(`mul\(\d+,\d+\)|do\(\)|don\'t\(\)`)
+	matches := re.FindAllString(memory, -1)
+	if matches == nil {
+		log.Println("Did not find matches")
+		return 0
+	}
+
+	count := 0
+
+	resultsCh := make(chan int)
+	wg := &sync.WaitGroup{}
+
+	process := true
+
+	for i := range matches {
+		if matches[i] == "do()" {
+			process = true
+		} else if matches[i] == "don't()" {
+			process = false
+		}
+		if process {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				result, _ := evaluateMulExpression(matches[i])
+				resultsCh <- result
+			}(i)
+		}
 	}
 
 	go func() {
