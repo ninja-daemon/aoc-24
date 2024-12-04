6188a528cb8e11c3a2d3c6d0613845868752682f
Jonathan Blanco Rodriguez <jblarod@mercadona.es>
2024-12-04 12:00:06 +0100

    Day 4 part 2 solution

--- day-04/main.go
+++ day-04/main.go
@@ -14,6 +14,11 @@ var directions = [8][2]int{
 	{1, -1}, {1, 1},
 }
 
+var xdirections = [4][2]int{
+	{-1, -1}, {-1, 1},
+	{1, -1}, {1, 1},
+}
+
 func main() {
 	url := "https://adventofcode.com/2024/day/4/input"
 
@@ -25,6 +30,10 @@ func main() {
 	solution1 := part1Solution(board, "XMAS")
 
 	fmt.Println("Part 1 solution:", solution1)
+
+	solution2 := part2Solution(board, "MAS")
+
+	fmt.Println("Part 2 solution:", solution2)
 
 }
 
@@ -61,6 +70,37 @@ func part1Solution(board [][]rune, word string) int {
 	return count
 }
 
+func part2Solution(board [][]rune, word string) int {
+	count := 0
+	resultsCh := make(chan bool)
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[i]); j++ {
+			if board[i][j] == rune(word[1]) {
+				wg.Add(1)
+				go func(i int) {
+					defer wg.Done()
+					resultsCh <- xSearch(board, word, i, j)
+				}(i)
+
+			}
+		}
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultsCh)
+	}()
+
+	for result := range resultsCh {
+		if result {
+			count++
+		}
+	}
+	return count
+}
+
 func search(board [][]rune, word string, row, col, index int, dir [2]int) bool {
 	if index == len(word) {
 		return true
@@ -72,4 +112,16 @@ func search(board [][]rune, word string, row, col, index int, dir [2]int) bool {
 	newRow, newCol := row+dir[0], col+dir[1]
 
 	return search(board, word, newRow, newCol, index+1, dir)
+}
+
+func xSearch(board [][]rune, word string, row, col int) bool {
+	counter := 0
+	for _, v := range xdirections {
+		dir := [2]int{v[0] * -1, v[1] * -1}
+		if search(board, word, row+v[0], col+v[1], 0, dir) {
+			counter++
+		}
+	}
+
+	return counter == 2
 }
